91d3187f3b70b5c3ef9a94d7ccdaad3f3a4b8e04
NgunyiGachie <antogachie@gmail.com>
2024-06-03 14:49:27 +0300

    Complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears VALUES (1, 'Mr.Chocolate', 10, 'F', 'brown', 'leader', 1);
+INSERT INTO bears VALUES (2, 'Rowdy', 15, 'M', 'grey', 'decisive', 1);
+INSERT INTO bears VALUES (3, 'Tabitha', 8, 'M', 'grey', 'strong-willed', 1);
+INSERT INTO bears VALUES (4, 'Sergeant Brown', 10, 'brown', 'M', 'leader', 0);
+INSERT INTO bears VALUES (5, 'Melissa', 2, 'F', 'grey', 'indecisive', 1);
+INSERT INTO bears VALUES (6, 'Grinch', 9, 'F', 'brown', 'leader', 0);
+INSERT INTO bears VALUES (7, 'Wendy', 5, 'M', 'grey', 'independent', 1);
+INSERT INTO bears VALUES (8, null, 1, 'F', 'grey', 'playful', 1);
+
